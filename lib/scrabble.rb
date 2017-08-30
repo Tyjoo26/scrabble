4789c94e41f25aa47176b5a9a917958e4f68d614
Tyjoo26 <Tyjoo21@gmail.com>
2017-08-30 09:57:33 -0600

    Finalized, still need to work on 10 pt bonus

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,14 +1,77 @@
 class Scrabble
 
-  attr_reader :word
+  attr_reader
 
-  def initialize(word)
-    @word = word
+  def initialize
+    @key = {
+      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+      "Y"=>4, "Z"=>10
+    }
   end
+
 
   def score(word)
-    1
+    if word == nil
+      return 0
+    elsif word.is_a? Integer
+      return 0
+    else
+      data = word.upcase.split("")
+      score_conversion_string(data)
+    end
   end
+
+  def score_conversion_string(data)
+    if data == []
+      return 0
+    else
+      data.map do |letter|
+        @key[letter]
+      end.inject(:+)
+    end
+  end
+
+
+  def convert_letters_to_array(data)
+    data.map do |letter|
+      @key[letter]
+    end
+  end
+
+
+  def score_with_multipliers(data, multipliers, double = 1)
+    data = data.upcase.split("")
+    if data.count >= 7
+      new_array = convert_letters_to_array(data)
+      new_array.zip(multipliers).map do |x,y|
+        x * y
+      end.inject(:+) + 10 * double
+    else
+      new_array = convert_letters_to_array(data)
+      new_array.zip(multipliers).map do |x,y|
+        x * y
+      end.inject(:+) * double
+    end
+  end
+
+
+
+
+
+end
+
+#First goal is to figure out a way to assign points based on the word being given to the progam. We'll need to define a method to connect each letter of a string to its assigned value in the method hash def point_values
+
+#Second goal is to define a method to add up the individual scores of letters into a total points
+
+##there is a distinction between letter and words. Each is an argument in itself.
+
+#define a score_with_multipliers method that can take multiple arguments. The first argument is the string, the second argument is the multiplier of score based on array, an optional third argument is passed as an optional final score multiplier
 
 #   Note: a double letter score in Scrabble is dependent on the position of a letter on the board. The arrays passed to `#score_with_multipliers` indicate the multiplier for a letter in the corresponding position of the word that is passed as the first argument.
 # ```
@@ -28,34 +91,3 @@ class Scrabble
 # => 58
 # ```
 # Use the existing `point_values` method in the `Scrabble` class as a source for each letter's value.
-
-  def point_values
-    {
-      "A"=>1, "B"=>3, "C"=>3, "D"=>2,
-      "E"=>1, "F"=>4, "G"=>2, "H"=>4,
-      "I"=>1, "J"=>8, "K"=>5, "L"=>1,
-      "M"=>3, "N"=>1, "O"=>1, "P"=>3,
-      "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
-      "U"=>1, "V"=>4, "W"=>4, "X"=>8,
-      "Y"=>4, "Z"=>10
-    }
-
-  def scrabble_points
-      letters = upcase.split('')
-
-      total = 0
-  end
-
-
-
-#First goal is to figure out a way to assign points based on the word being given to the progam. We'll need to define a method to connect each letter of a string to its assigned value in the method hash def point_values
-
-#Second goal is to define a method to add up the individual scores of letters into a total points
-
-##there is a distinction between letter and words. Each is an argument in itself.
-
-#define a score_with_multipliers method that can take multiple arguments. The first argument is the string, the second argument is the multiplier of score based on array, an optional third argument is passed as an optional final score multiplier
-
-
-
-end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -9,11 +9,55 @@ class ScrabbleTest < Minitest::Test
     assert_equal 1, Scrabble.new.score("a")
     assert_equal 4, Scrabble.new.score("f")
   end
-  #
-  # def test_scrabble_points
-  #   scrabble = Scrabble.new
-  #
-  #   assert_equal 
 
+  def test_it_can_score_a_string
+    scrabble = Scrabble.new
+    assert_equal 8, scrabble.score("hello")
   end
+
+  def test_it_can_score_a_different_string
+    scrabble = Scrabble.new
+    assert_equal 13, scrabble.score("sparkle")
+  end
+
+  def test_empty_string_is_zero
+    scrabble = Scrabble.new
+
+    assert_equal 0, scrabble.score("")
+  end
+
+  def test_nil_is_zero
+    scrabble = Scrabble.new
+
+    assert_equal 0, scrabble.score(nil)
+  end
+
+  def test_integer_returns_zero
+    scrabble = Scrabble.new
+
+    assert_equal 0, scrabble.score(55)
+  end
+
+  def test_score_with_multipliers
+    scrabble = Scrabble.new
+    actual = scrabble.score_with_multipliers('hello', [1,2,1,1,1])
+
+    assert_equal 9, actual
+  end
+
+  def test_score_with_multiplers_and_double_score
+    scrabble = Scrabble.new
+    actual = scrabble.score_with_multipliers('hello', [1,2,1,1,1], 2)
+
+    assert_equal 18, actual
+  end
+
+  def test_ten_point_score_bonus
+    scrabble = Scrabble.new
+    actual = scrabble.score_with_multipliers('sparkle', [1,2,1,3,1,2,1], 2)
+
+    assert_equal 58, actual
+  end
+
+
 end
